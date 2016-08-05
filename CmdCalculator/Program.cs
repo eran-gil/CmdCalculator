c38d63b3d0bf7f28d295b7f40bbe8a2e69688f94
erang <eran.gil2@gmail.com>
2016-08-05 21:43:03 +0300

    Added dependency injection to the calculator, for fun

--- CmdCalculator/Program.cs
+++ CmdCalculator/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using CmdCalculator.Evaluations;
 using CmdCalculator.Exceptions;
+using CmdCalculator.Interfaces.Evaluations;
+using CmdCalculator.Interfaces.Parsers;
+using CmdCalculator.Interfaces.Tokens;
 using CmdCalculator.Parsers;
 using CmdCalculator.Tokenization;
+using Microsoft.Practices.Unity;
 
 namespace CmdCalculator
 {
@@ -10,22 +14,33 @@ namespace CmdCalculator
     {
         static void Main()
         {
-            var calculatorFactory =
-                new BasicCalcualtorFactory<string, int>(
-                    new BasicTokenizerFactory<string, char>(
-                        new StringInputReaderFactory(),
-                        new CharTokenParsersProvider()
-                    ),
-                    new DefaultExpressionParsersProvider(), 
-                    new EvaluationVisitorFactory<int>(
-                        new IntegerEvaluatorProvider()
-                    )
-                );
+            var container = new UnityContainer();
+
+            IInputReaderFactory<string, char> stringReaderFactory = container.Resolve<StringInputReaderFactory>();
+            container.RegisterInstance(stringReaderFactory);
+
+            ITokenParsersProvider<char> tokenParsersProvider = container.Resolve<CharTokenParsersProvider>();
+            container.RegisterInstance(tokenParsersProvider);
+
+            ITokenizerFactory<string> tokenizerFactory = container.Resolve<BasicTokenizerFactory<string, char>>();
+            container.RegisterInstance(tokenizerFactory);
+
+            IExpressionParsersProvider expressionParsersProvider = container.Resolve<DefaultExpressionParsersProvider>();
+            container.RegisterInstance(expressionParsersProvider);
+
+            IExpressionEvaluatorProvider<int> evaluatorProvider = container.Resolve<IntegerEvaluatorProvider>();
+            container.RegisterInstance(evaluatorProvider);
+
+            IEvaluationVisitorFactory<int> evaluationVisitorFactory = container.Resolve<EvaluationVisitorFactory<int>>();
+            container.RegisterInstance(evaluationVisitorFactory);
+
+            var calculatorFactory = container.Resolve<BasicCalcualtorFactory<string, int>>();
 
             var calculator = calculatorFactory.CreateCalculator();
 
             while (true)
             {
+                Console.WriteLine("Please enter an expression for the calculator");
                 var input = Console.ReadLine();
                 if (input == "exit")
                 {
@@ -44,6 +59,7 @@ namespace CmdCalculator
                 }
 
                 Console.WriteLine(result);
+                Console.WriteLine();
             }
         }
     }
