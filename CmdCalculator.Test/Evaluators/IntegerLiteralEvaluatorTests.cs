0b2e539595f6a5b8353f56e032fb637f1243710f
Eran Gil <eran.gil2@gmail.com>
2016-09-11 21:18:40 +0300

    Added tests ffor the integer literal expression evaluator

---
+++ CmdCalculator.Test/Evaluators/IntegerLiteralEvaluatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CmdCalculator.Evaluations;
+using CmdCalculator.Expressions;
+using CmdCalculator.Interfaces.Evaluations;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace CmdCalculator.Test.Evaluators
+{
+    [TestFixture]
+    public class IntegerLiteralEvaluatorTests
+    {
+        [Test]
+        public void Evaluator_Evaluates_Literal_Expression_Correctly()
+        {
+            //Arrange
+            const int expectedResult = 6;
+            var evaluator = new IntegerLiteralExpressionEvaluator();
+            var expression = new LiteralExpression("6");
+            var visitor = A.Dummy<IEvaluationVisitor<int>>();
+
+            //Act
+            var result = evaluator.Evaluate(expression, visitor);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
